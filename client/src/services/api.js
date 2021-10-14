112e470038f5749f066b14831c055b611d421e00
robertthome <remedy42290@gmail.com>
2021-10-14 14:50:44 -0700

    api.js finished

---
+++ client/src/services/api.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+
+export const BASE_URL =
+  process.env.NODE_ENV === 'production' ? 'google.com' : 'http://localhost:5000'
+
+const Client = axios.create({ baseURL: BASE_URL })
+
+export default Client
