d90842fe43d60bd81c16266e841ea37931bd7985
robertthome <remedy42290@gmail.com>
2021-10-14 11:47:19 -0700

    auth.py finished

---
+++ resources/auth.py
@@ -0,0 +1,10 @@
+from flask_restful import Resource
+from flask import request
+from models.user import User
+
+class Login(Resource):
+    def post(self):
+        pass
+
+    def get(self):
+        pass
