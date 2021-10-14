79d022b2ff5c05b7f7ee3aefff2d2a2ba27a8bcc
robertthome <remedy42290@gmail.com>
2021-10-14 11:53:39 -0700

    resources import in app.py

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask_restful import Api
 from flask_cors import CORS
 from flask_migrate import Migrate
 from models.db import db
+from models.user import User
+from models.post import Post
+from resources.user import Users, UserDetail
+from resources.post import Posts, PostDetails
 
 app = Flask(__name__)
 
