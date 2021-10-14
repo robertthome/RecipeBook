f1d81c59a1c01fec12fb1c062ddc62d5a725aa4b
robertthome <remedy42290@gmail.com>
2021-10-14 14:51:38 -0700

    posts.js finished

---
+++ client/src/services/posts.js
@@ -0,0 +1,16 @@
+import Client from './api'
+
+export const GetPosts = async () => {
+  const res = await Client.get('/posts')
+  return res.data
+}
+
+export const CreatePost = async data => {
+  const res = await Client.post('/posts', data)
+  return res.data
+}
+
+export const DeletePost = async id => {
+  const res = await Client.delete(`/posts/${id}`)
+  return res
+}
