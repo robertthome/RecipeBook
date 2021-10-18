56a1238f9c45bba30dfb8a66f52575b7639c28b0
robertthome <remedy42290@gmail.com>
2021-10-18 08:16:19 -0700

    back to node_env

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,9 +2,9 @@ import axios from 'axios'
 
 const Client = axios.create({
   baseURL:
-    process.env.FLASK_ENV === 'production'
+    process.env.NODE_ENV === 'production'
       ? process.env.VUE_APP_API_URL
       : 'http://localhost:5000'
 })
-
+console.log(process.env.VUE_APP_API_URL)
 export default Client
